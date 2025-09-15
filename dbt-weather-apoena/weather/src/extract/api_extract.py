400d6edeadaf89eb444d3af64771d2eef850a143
Marcelo Ribas <marcelo.pachione@icloud.com>
2025-09-15 13:22:28 +0100

    feat: add connect db function

--- dbt-weather-apoena/weather/src/extract/api_extract.py
+++ dbt-weather-apoena/weather/src/extract/api_extract.py
@@ -11,6 +11,27 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+## DB Connection
+def connecto_to_database():
+    logger.info(f"Connecting to the database {os.getenv('POSTGRES_DB')}")
+
+    try:
+        conn = psycopg2.connect(
+            dbname=os.getenv('POSTGRES_DB'),
+            user=os.getenv('POSTGRES_USER'),
+            password=os.getenv('POSTGRES_PASSWORD'),
+            host=os.getenv('POSTGRES_HOST'),
+            port=5432,
+        )
+
+        logger.info(f"Sucessfully connect to the database {os.getenv('POSTGRES_DB')}")
+
+        return conn
+    except psycopg2.Error as e:
+        logger.error(f"Failed to connec to the database {os.getenv('POSTGRES_DB')}, error : {e}")
+        return None
+
+## Api Extract
 def get_weather_data(city: str):
     api_key = os.getenv('OPENWEATHER_API_KEY')
 
@@ -39,6 +60,9 @@ def get_weather_data(city: str):
 
         return None
 
+
 if __name__ == '__main__':
     city = 'Sao Paulo'
     get_weather_data(city)
+
+    connecto_to_database()
