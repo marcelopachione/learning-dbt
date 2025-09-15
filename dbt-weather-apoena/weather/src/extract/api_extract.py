aa912eecaa61a404c56e9ed6630324b5704c1c34
Marcelo Ribas <marcelo.pachione@icloud.com>
2025-09-15 12:04:08 +0100

    feat: add get_weather_data function to collect data from api

---
+++ dbt-weather-apoena/weather/src/extract/api_extract.py
@@ -0,0 +1,44 @@
+import os
+import logging
+import requests
+import psycopg2
+from dotenv import load_dotenv
+
+# Load envs
+load_dotenv()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def get_weather_data(city: str):
+    api_key = os.getenv('OPENWEATHER_API_KEY')
+
+    if not api_key:
+        logging.error("Api key not found")
+        return None
+    
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'unit': 'metric'
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+
+        weather_data = response.json()
+
+        logger.info(f"Weather data for {city}: {weather_data}")
+
+        return weather_data
+    except requests.RequestException as e:
+        logger.error(f"Error featching weather data: {e}")
+
+        return None
+
+if __name__ == '__main__':
+    city = 'Dublin'
+    get_weather_data(city)
